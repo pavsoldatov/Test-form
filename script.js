a9c82dbe8496f9cfc8be57ca273376718b9545e5
pavsoldatov <snowschoe@gmail.com>
2022-09-23 13:37:01 +0300

    Add readme

--- script.js
+++ script.js
@@ -15,19 +15,13 @@ const sortByValueButton = document.querySelector(".content__button#sortByValue")
 const showXmlButton = document.querySelector(".content__button#showXML");
 
 export const pairList = [];
-/*  This is our source of truth where we store key-value pairs as objects.
-    It reflects the key=value pairs rendered on screen, including deletion and sorts.
-    In this app, changes to "the state" of this array are made by way of MUTATION. */
+/*  This is our source of truth where we store key-value pairs as objects. */
 
 form.addEventListener("submit", handleFormSubmit);
 deleteButton.addEventListener("click", handleDelete);
 sortByNameButton.addEventListener("click", handleSortByName);
 sortByValueButton.addEventListener("click", handleSortByValue);
-/*  Both sorts use Intl.Collator to sort by name or value.
-    These sorts delete the old list of output.options entirely and populate (re-render), one-by-one,
-    all new options from the sorted pairList array into the <select> element. */
 showXmlButton.addEventListener("click", handleShowXml);
 
-
 /* uncomment this function to populate dummy pairs to output*/
-populateDummies();
+// populateDummies();
